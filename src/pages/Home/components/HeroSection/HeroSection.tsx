1d95dca651649668d834d4e8ac151e79c2744429
Md yasen rafe <mdyasenrafe@gmail.com>
2024-07-13 03:08:09 +0600

    carousel updated

--- src/pages/Home/components/HeroSection/HeroSection.tsx
+++ src/pages/Home/components/HeroSection/HeroSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
+import parse from "html-react-parser";
 
 // Import Swiper styles
 import "swiper/css";
@@ -20,34 +21,28 @@ import { colors } from "../../../../theme/color";
 export const HeroSection = () => {
   const sportsCarouselData = [
     {
-      image: "https://i.ibb.co/18JxH2C/hero-1.png", // Updated image URL for consistency
-      title: "Gear Up for the Marathon - 30% Off",
+      image: "https://i.ibb.co/18JxH2C/hero-1.png",
+      title: `Gear Up for the<br />Marathon`,
       description: "Save up to 30% on all marathon essentials!",
       link: "/category/marathon-gear",
     },
     {
-      image: "https://i.ibb.co/18JxH2C/hero-1.png",
-      title: "Find Your Zen",
+      image: "https://i.ibb.co/fYQwm8D/Cream-Beige-Food-Banner-Landscape-2.png",
+      title: `Find Your Zen with<br />Our Yoga Collection`,
       description: "Up to 25% off on yoga mats, blocks, and apparel.",
       link: "/category/yoga",
     },
     {
-      image: "https://i.ibb.co/18JxH2C/hero-1.png",
-      title: "Football Season Kickoff",
+      image: "https://i.ibb.co/gDhHY25/Cream-Beige-Food-Banner-Landscape-1.png",
+      title: `Football Season<br />Kickoff Sale`,
       description: "Get ready for the season with up to 40% off football gear!",
       link: "/category/football",
     },
     {
-      image: "https://i.ibb.co/18JxH2C/hero-1.png",
-      title: "Hit the Trails",
+      image: "https://i.ibb.co/VSjBWfJ/Cream-Beige-Food-Banner-Landscape-3.png",
+      title: `Hit the Trails with<br />Our Cycling Gear`,
       description: "Enjoy deals on bikes and accessories up to 35% off.",
       link: "/category/cycling",
-    },
-    {
-      image: "https://i.ibb.co/18JxH2C/hero-1.png",
-      title: "Boost Your Workout",
-      description: "Essential fitness gear at a discount of up to 50%!",
-      link: "/category/fitness",
     },
   ];
 
@@ -84,9 +79,9 @@ export const HeroSection = () => {
                 style={{
                   color: colors.secondary,
                 }}
-                className="text-4xl md:text-[64px] leading-[2.75rem] sm:leading-[2.75rem] lg:leading-[4.75rem]"
+                className="text-4xl md:text-[48px] lg:text-[64px] leading-[2.75rem] sm:leading-[2.75rem] md:leading-[3.50rem] lg:leading-[4.75rem]"
               >
-                {item.title}
+                {parse(item.title)}
               </Text>
               <Text variant={TextVariant.P1}>{item.description}</Text>
               <CustomButton
